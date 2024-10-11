6020c91d01a073ac7c44bc8705df5d5531c2dea8
Ederson <ederson.elias@>
2024-10-11 14:22:16 +0700

    Refactor: Renamed variables for improved clarity

--- src/bank/bank.test.ts
+++ src/bank/bank.test.ts
@@ -9,33 +9,33 @@ describe("Bank System", () => {
     });
 
     it("should allow a customer to join the bank", () => {
-        const customer = bank.addCustomer("Kanguru", 100);
+        const customer = bank.addCustomer("Kangaroo", 100);
         expect(customer.getBalance()).toBe(100);
     });
 
     it("should  not allow a customer to join the bank with a negative deposit", () => {
-        expect(() => bank.addCustomer("Kanguru", -1)).toThrow(ErrorMessages.InitialDepositNonNegative);
+        expect(() => bank.addCustomer("Kangaroo", -1)).toThrow(ErrorMessages.InitialDepositNonNegative);
     });
 
     it("should not found a non exist customer", () => {
-        expect(() => bank.getCustomer("Kanguru")).toThrow(ErrorMessages.CustomerNotFound);
+        expect(() => bank.getCustomer("Kangaroo")).toThrow(ErrorMessages.CustomerNotFound);
     });
 
     it("should not allow adding a customer with a duplicate name", () => {
-        bank.addCustomer("Kanguru", 100);
-        expect(() => bank.addCustomer("Kanguru", 200)).toThrow(ErrorMessages.CustomerAlreadyExists);
+        bank.addCustomer("Kangaroo", 100);
+        expect(() => bank.addCustomer("Kangaroo", 200)).toThrow(ErrorMessages.CustomerAlreadyExists);
     });
 
     it("should allow the bank to transfer money between customers", () => {
-        bank.addCustomer("Kanguru", 200);
+        bank.addCustomer("Kangaroo", 200);
         bank.addCustomer("Koala", 100);
-        bank.transferBetweenCustomers("Kanguru", "Koala", 50);
-        expect(bank.getCustomer("Kanguru").getBalance()).toBe(150);
+        bank.transferBetweenCustomers("Kangaroo", "Koala", 50);
+        expect(bank.getCustomer("Kangaroo").getBalance()).toBe(150);
         expect(bank.getCustomer("Koala").getBalance()).toBe(150);
     });
 
     it("should show the total balance of the bank", () => {
-        bank.addCustomer("Kanguru", 200);
+        bank.addCustomer("Kangaroo", 200);
         bank.addCustomer("Koala", 300);
         expect(bank.getTotalBalance()).toBe(500);
     });
